3df1a62b93600cd8a9ec5f3d5a74e4c909de1534
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-10-07 17:39:22 -0400

    set up file strutcture and added schema and seed sql

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+CREATE TABLE burgers
+(
+    id int
+    AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar
+    (30) NOT NULL,
+  devoured BOOLEAN DEFAULT FALSE
+);
+
+
+
+
+
+
+
+
