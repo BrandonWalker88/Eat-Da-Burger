37b059c87303f793a0b46f7c37e0ba2bf82a3015
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-10-07 17:40:02 -0400

    fixed bugs

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('baconburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('hamburger', false);
