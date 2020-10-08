1bfe1e4e434f8220dba21c2c1416320e0be8f9c5
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-10-08 18:45:16 -0400

    added content to connection, controllers, orm, index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,32 @@ const burgers = require("../models/burger");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  burgers.selectAll((data) => {
+    const burObject = {
+      burger: data,
+    };
+    res.render("index", burObject);
+  });
+});
+
+router.post("/", (req, res) => {
+  burger.insertOne(["burger_name"], [req.body.burger_name], (data) => {
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+  burger.updateOne(
+    {
+      devoured: true,
+    },
+    condition,
+    (data) => {
+      res.redirect("/");
+    }
+  );
+});
+
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,26 @@
 const orm = require("../config/orm");
+// Create the burger object
+const burger = {
+  // Select all burger table entries
+  selectAll: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+
+  // The variables cols and vals are arrays
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+};
 
 module.exports = burger;
